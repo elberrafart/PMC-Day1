43c090cfe6abd27d58af6bb152c4f338aff9e848
Raz <rafartelber@gmail.com>
2023-03-26 20:01:07 -0500

    inserted gui

--- bonus/bonus.py
+++ bonus/bonus.py
@@ -1,6 +1,48 @@
-from bonus.bonus2 import get_maximum
+import PySimpleGUI as sg
 
-celsius = get_maximum()
 
-fahrenheit = celsius * 1.8 + 32
-print(fahrenheit)
+def convert(feet, inches):
+    meters = feet * 0.3048 + inches * 0.0254
+    return meters
+
+
+sg.theme("Black")
+
+feet_label = sg.Text("Enter feet: ")
+feet_input = sg.Input(key="feet")
+
+inches_label = sg.Text("Enter inches: ")
+inches_input = sg.Input(key="inches")
+
+button = sg.Button("Convert")
+output_label = sg.Text("", key="output")
+exit_button = sg.Button("Exit")
+
+window = sg.Window("Convertor",
+                   layout=[[feet_label, feet_input],
+                           [inches_label, inches_input],
+                           [button, exit_button, output_label]])
+
+while True:
+    event, values = window.read()
+    match event:
+        case "Exit":
+            break
+        case "Convert":
+            try:
+                feet = float(values["feet"])
+                inches = float(values["inches"])
+                result = convert(feet, inches)
+                window["output"].update(value=f"{result} m", text_color="white")
+            except ValueError:
+                sg.popup("Please provide two numbers.", font=("Helvetica", 20))
+
+        case sg.WIN_CLOSED:
+            break
+    feet = float(values["feet"])
+    inches = float(values["inches"])
+
+    result = convert(feet, inches)
+    window["output"].update(value=f"{result} m", text_color="white")
+
+window.close()
